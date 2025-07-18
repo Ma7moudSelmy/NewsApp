2f0a2ff139a8fd3a510470fffbd1350b5d5b46ba
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-07-18 11:30:41 +0300

    Add Articlesmodels class and refactor news fetching in Newsservices

---
+++ lib/Models/articlesModels.dart
@@ -0,0 +1,8 @@
+class Articlesmodels {
+ final String? image;
+ final String title;
+ final String? subtitle;
+ Articlesmodels( {
+  
+   required this.image,required this.title,required this.subtitle});
+}  
--- lib/services/Newsservices.dart
+++ lib/services/Newsservices.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:news_app/Models/articlesModels.dart';
 
 class Newsservices {
   final Dio dio;
@@ -6,13 +7,20 @@ class Newsservices {
   Newsservices(this.dio);
 
   getNews() async {
-  var response  = await dio.get(
-      "https://newsapi.org/v2/top-headlines?apiKey=972bd20cf6cf4722b80aba171a70528d&country=us&category=general"
+    var response = await dio.get(
+      "https://newsapi.org/v2/top-headlines?apiKey=972bd20cf6cf4722b80aba171a70528d&country=us&category=general",
+    );
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articles = jsonData["articles"];
+    List<Articlesmodels> articlesList = [];
+    for (var article in articles) {
+      Articlesmodels articlesmodels = Articlesmodels(
+        image: article["urlToImage"],
+        title: article["title"],
+        subtitle: article["description"],
       );
-   Map<String,dynamic> jsonData=  response.data;
-   List<dynamic> articles = jsonData["articles"];
-   print( articles);
+      articlesList.add(articlesmodels);
+    }
+    print(articlesList);
   }
 }
-
-  
